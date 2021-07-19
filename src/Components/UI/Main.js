0fa0516d8419152bac5fb75e182bbca23727ca30
PervertKun <shivenderkumar761@gmail.com>
2021-07-19 13:53:07 +0530

    unused vars fixed

--- src/Components/UI/Main.js
+++ src/Components/UI/Main.js
@@ -2,7 +2,6 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { Search, CurrentWeather, Forecast } from "./SubComponents/index";
 function Main() {
-  const weather = useSelector((state) => state.weather);
   return (
     <div className="Main full-w-h">
       <Search />
